a2072a5e7d15463db24946a89eaebfca9b9529c2
holysik <kyusik0548@naver.com>
2025-12-15 18:12:32 +0900

    채팅 화면 작업중

---
+++ Devlog/src/main/resources/static/js/chatting/chatting.js
@@ -0,0 +1,31 @@
+/* 선택 효과 주기 */
+const listItem = document.getElementsByClassName('room-item')
+
+for (let item of listItem) {
+
+    item.addEventListener('click', e => {
+
+        for (let el of listItem) {
+            el.classList.remove('is-selected')
+            
+        }
+        
+        item.classList.add('is-selected')
+    })
+    
+}
+
+document.addEventListener('DOMContentLoaded', e => {
+    scrollToBottom()
+})
+
+
+
+/* 메세지 하단 함수 */
+const messageArea = document.getElementsByClassName('message-list')[0]
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
+
+
