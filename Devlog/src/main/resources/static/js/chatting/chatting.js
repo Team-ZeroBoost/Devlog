2a4382f3c82ff9fb42b6b4379a3ae1d282f68db3
holysik <kyusik0548@naver.com>
2025-12-16 18:14:49 +0900

    채팅 작업 화면

--- Devlog/src/main/resources/static/js/chatting/chatting.js
+++ Devlog/src/main/resources/static/js/chatting/chatting.js
@@ -1,3 +1,4 @@
+
 /* 선택 효과 주기 */
 const listItem = document.getElementsByClassName('room-item')
 
@@ -28,6 +29,61 @@ function scrollToBottom() {
     messageArea.scrollTop = messageArea.scrollHeight
 }
 
+
+/*채팅방 추가 버튼 클릭 시 */
+
+const chatAddBtn = document.getElementById('chat-add-btn');
+const createRoom = document.querySelector('.create-room');
+
+chatAddBtn.addEventListener('click', () => {
+    createRoom.classList.toggle('hide');
+});
+
+
+/* 유저 선택 시 */
+const radioCheck = document.getElementsByName('invite')
+const userList = document.getElementsByClassName('select-user-list')[0]
+
+for (let item of radioCheck) {
+
+    item.addEventListener("change", e => {
+
+        const followItem =  e.target.closest('.follow-item');
+        const userName = followItem.querySelector('.name').innerText;
+        console.log(userName)
+
+        
+        const div = document.createElement('div')
+        const span = document.createElement('span')
+        const deleteBtn = document.createElement('span')
+        div.classList.add('user-item')
+        span.innerHTML = userName
+        deleteBtn.classList.add('list-delete-btn')
+        deleteBtn.innerText = ' x'
+
+        userList.innerHTML = "";
+
+        div.append(span, deleteBtn);
+        userList.appendChild(div);
+
+
+        deleteBtn.addEventListener('click', e => {
+            item.checked = false;
+            userList.innerHTML = "";
+        })
+
+    })
+    
+}
+
+
+/* 개인 그룹 선택 */
+const private = document.querySelector('.private');
+const group = document.querySelector('.group');
+
+private.addEventListener('click', e=>{
+    private.classList
+})
 
 
 
