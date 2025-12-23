ba62c26b1f440294c36c68b37fa90b12e5208140
holysik <kyusik0548@naver.com>
2025-12-23 15:54:26 +0900

    Update. DBConfig 파일 에러 수정

---
+++ Devlog/src/main/java/com/devlog/project/chatting/dto/ChattingDTO.java
@@ -0,0 +1,37 @@
+package com.devlog.project.chatting.dto;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+public class ChattingDTO {
+	
+	
+	
+	
+	@Getter
+	@Setter
+	@ToString
+	public static class ChattingListDTO {
+	    private Long chattingRoomNo;
+	    private String roomType;
+	    private String pinnedYn;
+	    private String displayName;
+	    private String roomImg;
+	    private String lastMessage;
+	    private LocalDateTime lastMessageAt;
+	    private Long unreadCount;
+
+
+	}
+
+
+}
+
