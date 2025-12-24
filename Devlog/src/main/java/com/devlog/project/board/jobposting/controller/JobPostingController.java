3c820a91158e319e8025520c6ae22a86e6e95bc8
Juyeon <yeon06200000@gmail.com>
2025-12-24 18:12:03 +0900

    Feat.채용공고 로직 수정

--- Devlog/src/main/java/com/devlog/project/board/jobposting/controller/JobPostingController.java
+++ Devlog/src/main/java/com/devlog/project/board/jobposting/controller/JobPostingController.java
@@ -1,11 +1,17 @@
 package com.devlog.project.board.jobposting.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.devlog.project.board.jobposting.dto.JobPostingDTO;
 import com.devlog.project.board.jobposting.service.JobPostingService;
 
 @Controller
@@ -15,9 +21,14 @@ public class JobPostingController {
 	private JobPostingService jobPostingService;
 	
 	
-	// 채용공고 캘린더 이동	
+	// 채용공고 캘린더 화면 전환
 	@GetMapping("/jobposting")
-	public String jobposting() {
+	public String jobposting(
+			Model model) {
+		List<JobPostingDTO> jobcalender = jobPostingService.selectjoblist();
+		System.out.println(jobcalender);
+		model.addAttribute("jobcalender", jobcalender);
+		
 		return "board/Jobposting/calender";
 	}
 	
@@ -27,4 +38,8 @@ public class JobPostingController {
 	public void JobCrawler() {
 		jobPostingService.JobCrawler();
 	}
+	
+	
+	
+	
 }
