812b5d6d4535c63eb133c1783f4c0107af132f96
nickName333777 <yypark.rok@gmail.com>
2026-01-16 07:47:24 +0900

    FIX. Integer findByCurrentLevel(@Param(currentExp) Integer currentExp);로 메서드 파라미터 명시하여 로그인시 발생하는 java compiler 런타임에러 fix

--- Devlog/src/main/java/com/devlog/project/member/model/repository/LevelRepository.java
+++ Devlog/src/main/java/com/devlog/project/member/model/repository/LevelRepository.java
@@ -2,6 +2,7 @@ package com.devlog.project.member.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devlog.project.member.model.entity.Level;
 
@@ -16,6 +17,7 @@ public interface LevelRepository extends JpaRepository<Level, Integer>{
 			from Level lv
 			where lv.requiredTotalExp <= :currentExp
 			""")
-	Integer findByCurrentLevel(Integer currentExp);
+	//Integer findByCurrentLevel(Integer currentExp); // 로그인 런타임오류 발생 d
+	Integer findByCurrentLevel(@Param("currentExp") Integer currentExp);
 
 }
